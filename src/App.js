936738fd20b4c74121f9568ae7d33c3cbab23052
alexandre.froger <alexandre.froger@numeko.fr>
2023-02-28 20:02:52 +0100

    Fix error when run npm start because of newer version of node
    eslint, package dependy, redux-sage init deprecated...

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { store, sagaMiddleware } from './store';
+import rootSaga from './sagas';
+
+import Header from './components/Header';
+import InterventionsList from './containers/InterventionsList';
+
+sagaMiddleware.run(rootSaga);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="container">
+          <Header />
+          <Switch>
+            <Route path="/" exact>
+              <InterventionsList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Implémente le tableau ici/i);
+  expect(linkElement).toBeInTheDocument();
+});
